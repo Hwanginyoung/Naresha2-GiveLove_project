c1c66f296b05af8eb11d434117e866bbdf0aa3d6
Hwanginyoung <hwang5375@gmail.com>
2022-09-18 16:31:37 +0900

    header

--- givelove/src/containers/Header.js
+++ givelove/src/containers/Header.js
@@ -1,18 +1,26 @@
 import React from 'react';
+import NavLogo from '../image/NavLogo.png';
 import "./Header.css";
+import {Link, useNavigate} from 'react-router-dom';
 
 function Header() {
+
+  let navigate=useNavigate();
+
   return (
     <>
     <div id="nav-container">
-        <div id="logo">
-
-        </div>
-        <div id="menu">
-            <div>홈</div>
-            <div>기부</div>
-            <div>MY</div>
-        </div>
+      <div id="logo">
+        <img src={NavLogo} id="NavLogo" alt="NavLogo" />
+      </div>
+      <div id="MenuBar">
+        <div class="menu" onClick={()=>{ navigate('/home') }}>홈</div>
+        <div class="menu" onClick={()=>{ navigate('/donate') }}>기부</div>
+        <div class="menu" onClick={()=>{ navigate('/mypage') }}>MY</div>
+      </div>
+      <div id="member">
+        <div id="login" Link to='/login'>로그인</div>
+      </div>
     </div>
     </>
   );
