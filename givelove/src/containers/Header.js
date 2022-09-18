5e8f9bafa2710c64c75676270d076584f160a80a
Hwanginyoung <hwang5375@gmail.com>
2022-09-18 15:00:33 +0900

    set

---
+++ givelove/src/containers/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+    <div id="nav-container">
+        <div id="logo">
+
+        </div>
+        <div id="menu">
+            <div>홈</div>
+            <div>기부</div>
+            <div>MY</div>
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default Header;
