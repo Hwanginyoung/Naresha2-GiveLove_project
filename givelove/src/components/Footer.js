060320ea5957070c2691f09258f01c27eaac15e0
Hwanginyoung <hwang5375@gmail.com>
2022-09-18 15:04:34 +0900

    footer

--- givelove/src/components/Footer.js
+++ givelove/src/components/Footer.js
@@ -1,12 +1,16 @@
 import footerlogo from '../image/footerlogo.png';
 import './Footer.css';
 
-const footer = () => {
-    <div className='Footer'>
-        <header>
-            <img src={footerlogo} className="logo" alt="logo" />
-        </header>
-    </div>
+const Footer = () => {
+    return (
+        <>
+            <div className='Footer'>
+                <header>
+                    <img src={footerlogo} className="logo" alt="logo" />
+                </header>
+            </div>
+        </>
+    )
 }
 
-export default footer;
+export default Footer;
