a72cdf51f4ced42ca43fd59cc19c8c804044689e
Hwanginyoung <hwang5375@gmail.com>
2022-09-21 20:41:16 +0900

    signup

--- givelove/src/containers/Signup.js
+++ givelove/src/containers/Signup.js
@@ -16,17 +16,18 @@ function Signup() {
             <img src={BlackLogo} id="BlackLogo" alt="BlackLogo" />
             <p id="signupText">회원가입</p>
             <div id="id">아이디</div>
-            <input></input>
-            <button>중복확인</button>
+            <input id="idInput"></input>
+            <button id="ck">중복확인</button>
             <div id="password">비밀번호</div>
-            <input type="password"></input>
+            <input id="inputTxt" type="password"></input>
             <div>이름</div>
-            <input></input>
+            <input id="inputTxt"></input>
             <div>전화번호</div>
-            <input></input>
+            <input id="inputTxt" type="num" placeholder='숫자만 입력'></input>
             <br />
             <button id="signupButton">회원가입</button>
             <p id="Q">이미 회원이신가요?<button onClick={()=>{ navigate('/login') }}>로그인</button></p>
+            <p id="Q">단체 회원가입이신가요?<button onClick={()=>{ navigate('/Gsignup') }}>단체 회원가입</button></p>
         </div>
     </div>
     </>
