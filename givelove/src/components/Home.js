a9b30001cd7b5df6dacdf42bd9b933a11b246e06
Hwanginyoung <hwang5375@gmail.com>
2022-09-21 20:52:14 +0900

    home

---
+++ givelove/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import homeExp from '../image/homeExp.png';
+
+function Home() {
+
+  return (
+    <>
+    <div id="home">
+        <img src={homeExp} id="homeExp" alt='homeExp' />
+    </div>
+    </>
+  );
+}
+
+export default Home;
