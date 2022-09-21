8c2e2ad5c9f239eab7d0a40d2919b8cbae635458
bomin-1206 <gbm4655@naver.com>
2022-09-21 17:33:27 +0900

    Header

--- givelove/src/containers/Header.js
+++ givelove/src/containers/Header.js
@@ -8,19 +8,19 @@ function Header() {
   let navigate=useNavigate();
 
   return (
-    <>
-    <div id="nav-container">
-      <div id="logo">
-        <img src={NavLogo} id="BlackLogo" alt="BlackLogo" />
-      </div>
-      <div id="MenuBar">
-        <p className='menu' onClick={()=>{ navigate('/home') }}>홈</p>
-        <p className='menu' onClick={()=>{ navigate('/donate') }}>기부</p>
-        <p className='menu' onClick={()=>{ navigate('/mypage') }}>MY</p>
-        <p className='login' Link to='/login'>로그인</p>
+    <div className='Header'>
+      <div className='nav-container'>
+        <div className='logo'>
+          <img src={NavLogo} className='BlackLogo' alt="BlackLogo" />
+        </div>
+        <div className='MenuBar'>
+          <p className='menu' onClick={()=>{ navigate('/home') }}>홈</p>
+          <p className='menu' onClick={()=>{ navigate('/donate') }}>기부</p>
+          <p className='menu' onClick={()=>{ navigate('/mypage') }}>MY</p>
+          <p className='login' Link to='/login'>로그인</p>
+        </div>
       </div>
     </div>
-    </>
   );
 }
 
