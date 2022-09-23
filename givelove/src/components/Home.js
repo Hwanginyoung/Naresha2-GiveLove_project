565e0c927ddd4de40c700f446942983375878388
Hwanginyoung <hwang5375@gmail.com>
2022-09-23 15:13:36 +0900

    home

--- givelove/src/components/Home.js
+++ givelove/src/components/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import homeExp from '../image/homeExp.png';
+import "./Home.css";
 
 function Home() {
 
   return (
     <>
-    <div id="home">
+      <div id="home">
         <img src={homeExp} id="homeExp" alt='homeExp' />
-    </div>
+      </div>
     </>
   );
 }
