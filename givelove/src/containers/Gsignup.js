e6012b9a69253418a1fd51502302cc4a066c3641
Hwanginyoung <hwang5375@gmail.com>
2022-09-23 15:13:45 +0900

    gsignup

---
+++ givelove/src/containers/Gsignup.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Gsignup.css";
+
+function Gsignup() {
+
+  return (
+    <>
+      <div id="gsignup">
+        
+      </div>
+    </>
+  );
+}
+
+export default Gsignup;
