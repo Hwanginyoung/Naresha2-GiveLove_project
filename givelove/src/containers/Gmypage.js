708b139184b502d8f10b5c3f3e4acc47498df9d9
Hwanginyoung <hwang5375@gmail.com>
2022-09-25 17:05:04 +0900

    Gmy

---
+++ givelove/src/containers/Gmypage.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Gmypage.css";
+
+import Mypageheader from "../components/Mypageheader";
+
+function Gmypage() {
+  return (
+    <>
+      <Mypageheader />
+        <div id="gmypage">
+            <div id="information">
+                <p>단체정보</p>
+            </div>
+            <div id="Groupinfo">
+                
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Gmypage;
