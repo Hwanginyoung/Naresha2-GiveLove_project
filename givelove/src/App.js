3fe93a2ac42fb9d5d9a549a7eedbd3832b7f239f
bomin-1206 <gbm4655@naver.com>
2022-09-26 08:45:56 +0900

    App.js

--- givelove/src/App.js
+++ givelove/src/App.js
@@ -7,13 +7,10 @@ import Login from "./containers/Login";
 import Main from "./containers/Main";
 import Signup from "./containers/Signup";
 import Gsignup from "./containers/Gsignup";
-<<<<<<< Updated upstream
 import Home from "./components/Home";
 import Dmypage from "./containers/Dmypage";
 import Gmypage from "./containers/Gmypage";
-=======
 import MoneyDetail from "./containers/MoneyDetail";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -24,13 +21,10 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/gsignup" element={<Gsignup />} />
-<<<<<<< Updated upstream
         <Route path="/home" element={<Home />} />
         <Route path="/dmypage" element={<Dmypage />} />
         <Route path="/gmypage" element={<Gmypage />} />
-=======
         <Route path="/moneydetail" element={<MoneyDetail />} />
->>>>>>> Stashed changes
       </Routes>
       <Footer />
     </div>
