5c36a4e729426f093218d9dbb1d05521c24f68f9
Hwanginyoung <hwang5375@gmail.com>
2022-09-29 14:34:53 +0900

    donatemoney

--- givelove/src/containers/DonateMoney.js
+++ givelove/src/containers/DonateMoney.js
@@ -1,27 +1,29 @@
 import React from "react";
 import "./DonateMoney.css";
 
+import Donateheader from "../components/Donateheader";
 
 function DonateMoney() {
   return (
     <>
-        <div id="donatemoney">
-            <div id="userinfo">
-                <div id="text">기부자 정보</div>
-                <div id="name"></div>
-                <div id="birth"></div>
-                <div id="num"></div>
-            </div>
-            <div id="payment">
-                <div id="pay">기부금 결제</div>
-                <div id="money"></div>
-                <div id="bank"></div>
-                <div id="account"></div>
-                <div id="accountName"></div>
-                <div id="accountBirth"></div>
-                <div id="address"></div>
-            </div>
+      <Donateheader />
+      <div id="donatemoney">
+        <div id="userinfo">
+          <div id="text">기부자 정보</div>
+          <div id="name"></div>
+          <div id="birth"></div>
+          <div id="num"></div>
         </div>
+        <div id="payment">
+          <div id="text">기부금 결제</div>
+          <div id="money"></div>
+          <div id="bank"></div>
+          <div id="account"></div>
+          <div id="accountName"></div>
+          <div id="accountBirth"></div>
+          <div id="address"></div>
+        </div>
+      </div>
     </>
   );
 }
