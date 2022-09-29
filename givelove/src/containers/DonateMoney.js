c0fd0a206a156c7bf459bbb318c16b669a388911
Hwanginyoung <hwang5375@gmail.com>
2022-09-29 10:10:10 +0900

    donatemoney

--- givelove/src/containers/DonateMoney.js
+++ givelove/src/containers/DonateMoney.js
@@ -5,7 +5,14 @@ import "./DonateMoney.css";
 function DonateMoney() {
   return (
     <>
+        <div id="donatemoney">
+            <div id="userinfo">
+                
+            </div>
+            <div id="payment">
 
+            </div>
+        </div>
     </>
   );
 }
