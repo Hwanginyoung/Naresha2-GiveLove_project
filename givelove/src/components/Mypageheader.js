96e835c58660b89780222b02b298bde345126094
Hwanginyoung <hwang5375@gmail.com>
2022-09-29 14:34:44 +0900

    mypageheader

--- givelove/src/components/Mypageheader.js
+++ givelove/src/components/Mypageheader.js
@@ -4,7 +4,7 @@ import "./Mypageheader.css";
 
 function Mypageheader() {
   return (
-    <div id="headerbox">
+    <div id="myheaderbox">
         <div id="myheader">
             <p>MY페이지에서<br/>회원정보를 확인하고 수정 할 수 있습니다.</p>
             <button>로그아웃</button>
