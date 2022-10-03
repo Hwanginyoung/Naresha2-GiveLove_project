cde5d4ddd405b81cf65fb6ea9b8d4896320359dd
Hwanginyoung <hwang5375@gmail.com>
2022-10-03 23:15:26 +0900

    donatemoney

--- givelove/src/containers/DonateMoney.js
+++ givelove/src/containers/DonateMoney.js
@@ -10,18 +10,17 @@ function DonateMoney() {
       <div id="donatemoney">
         <div id="userinfo">
           <div id="text">기부자 정보</div>
-          <div id="name"></div>
-          <div id="birth"></div>
-          <div id="num"></div>
+          <div class="info">기부자명 <input></input> </div> 
+          <div class="info">생년월일 <input></input> </div>
+          <div class="info">전화번호 <input></input> </div>
         </div>
         <div id="payment">
           <div id="text">기부금 결제</div>
-          <div id="money"></div>
-          <div id="bank"></div>
-          <div id="account"></div>
-          <div id="accountName"></div>
-          <div id="accountBirth"></div>
-          <div id="address"></div>
+          <div class="info">기부금액 <input></input> </div>
+          <div class="info">은행명 <input></input> </div>
+          <div class="info">계좌번호 <input></input> </div>
+          <div class="info">예금주 성명 <input></input> </div>
+          <div class="info">예금주 생년월일 <input></input> </div>
         </div>
       </div>
     </>
