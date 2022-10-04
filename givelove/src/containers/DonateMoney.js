e0a70b8fb2da77f7af0cefdd9e5a791afde1c720
Hwanginyoung <hwang5375@gmail.com>
2022-10-04 22:03:47 +0900

    donatemoney

--- givelove/src/containers/DonateMoney.js
+++ givelove/src/containers/DonateMoney.js
@@ -9,25 +9,48 @@ function DonateMoney() {
       <Donateheader />
       <div id="donatemoney">
         <div id="userinfo">
-          {/* <div id="text">기부자 정보</div>
-          <div class="info">기부자명 <input></input> </div> 
-          <div class="info">생년월일 <input></input> </div>
-          <div class="info">전화번호 <input></input> </div> */}
-
+          <div id="text">기부자 정보</div>
           <div class="info">
-            <div>기부자명</div>
-            <input></input>
+            <div>기부자명 </div>
+            <input></input> 
+          </div> 
+          <div class="info">
+            <div>생년월일 </div>
+            <input></input> 
           </div>
-
+          <div class="info">
+            <div>전화번호 </div>
+            <input></input> 
+            <p id="numtext">‘-’ 를 제외한 숫자만 입력해 주세요.</p>
+          </div>
         </div>
+
         <div id="payment">
           <div id="text">기부금 결제</div>
-          <div class="info">기부금액 <input></input> </div>
-          <div class="info">은행명 <input></input> </div>
-          <div class="info">계좌번호 <input></input> </div>
-          <div class="info">예금주 성명 <input></input> </div>
-          <div class="info">예금주 생년월일 <input></input> </div>
+          <div class="info">
+            <div>기부금액 </div>
+            <input></input>
+            <p id="amounttext">원</p>
+          </div>
+          <div class="info">
+            <div>은행명 </div>
+            <input></input> 
+          </div>
+          <div class="info">
+            <div>계좌번호 </div>
+            <input></input> 
+            <p id="acnumtext">※ 기호 “-”없이 숫자만 입력하세요.<br/>휴대폰 번호로 된 계좌는 자동이체 되지 않습니다.</p>
+          </div>
+          <div class="info">
+            <div>예금주 성명 </div>
+            <input></input> 
+          </div>
+          <div class="info">
+            <div>예금주 생년월일 </div>
+            <input></input> 
+          </div>
         </div>
+        <button>기부하기</button>
       </div>
     </>
   );
