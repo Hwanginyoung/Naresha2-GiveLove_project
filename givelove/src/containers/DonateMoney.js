f7e21fa5cbe297f06b819f6408c54800003c3acb
Hwanginyoung <hwang5375@gmail.com>
2022-10-04 23:44:19 +0900

    donatemoney

--- givelove/src/containers/DonateMoney.js
+++ givelove/src/containers/DonateMoney.js
@@ -16,11 +16,11 @@ function DonateMoney() {
           </div> 
           <div class="info">
             <div>생년월일 </div>
-            <input></input> 
+            <input placeholder="ex) oooo.oo.oo"></input> 
           </div>
           <div class="info">
             <div>전화번호 </div>
-            <input></input> 
+            <input placeholder="ex) 01012345678"></input> 
             <p id="numtext">‘-’ 를 제외한 숫자만 입력해 주세요.</p>
           </div>
         </div>
@@ -34,7 +34,41 @@ function DonateMoney() {
           </div>
           <div class="info">
             <div>은행명 </div>
-            <select></select> 
+            <select>
+              <option>은행선택</option>
+              <optgroup class="text" label="국내은행" />
+              <option value="국민은행">국민은행</option>
+              <option value="기업은행">기업은행</option>
+              <option value="농협은행">농협은행</option>
+              <option value="신한은행">신한은행</option>
+              <option value="산업은행">산업은행</option>
+              <option value="우리은행">우리은행</option>
+              <option value="한국씨티은행">한국씨티은행</option>
+              <option value="하나은행">하나은행</option>
+              <option value="SC제일은행">SC제일은행</option>
+              <option value="경남은행">경남은행</option>
+              <option value="광주은행">광주은행</option>
+              <option value="대구은행">대구은행</option>
+              <option value="부산은행">부산은행</option>
+              <option value="새마을금고">새마을금고</option>
+              <option value="신협중앙회">신협중앙회</option>
+              <option value="우체국">우체국</option>
+              <option value="전북은행">전북은행</option>
+              <option value="제주은행">제주은행</option>
+              <option value="케이뱅크">케이뱅크</option>
+              <option value="토스뱅크">토스뱅크</option>
+              <optgroup />
+              <optgroup class="text" label="해외은행" />
+              <option value="도이치은행">도이치은행</option>
+              <option value="뱅크오브아메리카">뱅크오브아메리카</option>
+              <option value="중국건설은행">중국건설은행</option>
+              <option value="중국공상은행">중국공상은행</option>
+              <option value="중국은행">중국은행</option>
+              <option value="BNP파리바은행">BNP파리바은행</option>
+              <option value="HSBC은행">HSBC은행</option>
+              <option value="JP모간체이스은행">JP모간체이스은행</option>
+              <optgroup />
+            </select>
           </div>
           <div class="info">
             <div>계좌번호 </div>
@@ -47,7 +81,7 @@ function DonateMoney() {
           </div>
           <div class="info">
             <div>예금주 생년월일 </div>
-            <input></input> 
+            <input placeholder="ex) oooo.oo.oo"></input> 
           </div>
         </div>
         <button>기부하기</button>
